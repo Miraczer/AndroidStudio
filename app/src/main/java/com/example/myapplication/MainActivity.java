b9b34c1bdf153c2bcf3141312ebc87f0bc480b68
Mark Daniel <markdanhann123@gmail.com>
2023-02-11 17:50:59 +0800

    Initial MyApplication project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "This is a verbose log.");
+        Log.d(TAG, "This is debug log.");
+        Log.i(TAG, "This is an info log.");
+        Log.w(TAG, "This is warn log.");
+        Log.e(TAG, "This is an error log");
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        /*Intent intent = new Intent(MainActivity.this. SecondActivity.java);
+        starActivity(intent);*/
     }
 }
