ddfe482a1f6f156458d3b6eb05347e137474cc18
Admin <mirakelmasim@gmail.com>
2023-02-18 18:27:55 +0800

    Date: February 18, 2023 Task: Linear Layout, Relative Layout and Constraint Layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Log.w(TAG, "This is warn log.");
         Log.e(TAG, "This is an error log");
 
-        Button button = (Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.buttonmain);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
