f020ea2c6ff20744c16113899eeaecb350a2cde9
Admin <mirakelmasim@gmail.com>
2023-02-18 18:27:55 +0800

    Date: February 18, 2023 Task: Linear Layout, Relative Layout and Constraint Layout

--- app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -3,37 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
+
     private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_third);
 
-        Log.v(TAG, "This is a verbose log.");
-        Log.d(TAG, "This is debug log.");
-        Log.i(TAG, "This is an info log.");
-        Log.w(TAG, "This is warn log.");
-        Log.e(TAG, "This is an error log");
-
-        Button button = (Button) findViewById(R.id.buttonmain);
+        Button button = (Button) findViewById(R.id.buttonthree);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "Button Clicked");
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent = new Intent(ThirdActivity.this, ThirdActivity.class);
                 startActivity(intent);
             }
         });
-        /*Intent intent = new Intent(MainActivity.this. SecondActivity.java);
-        starActivity(intent);*/
     }
 }
