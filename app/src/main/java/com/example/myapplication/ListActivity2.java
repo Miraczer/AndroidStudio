5ee5b1928f7e1b343e96c713ea0d8b7ec2d6554b
Mark Daniel Musngi <mirakelmasim@gmail.com>
2023-03-04 18:31:52 +0800

    March 4, 2023

---
+++ app/src/main/java/com/example/myapplication/ListActivity2.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.*;
+
+public class ListActivity2 extends AppCompatActivity {
+
+    ListView listView;
+    TextView textView;
+
+    String[] listItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        listView=(ListView)findViewById(R.id.listView);
+        listItem= getResources().getStringArray(R.array.characters);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.my_list, R.id.textView, listItem);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), value, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
