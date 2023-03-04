d3856b5b9f01caae8414ca62c4923c96aa4cce6a
Mark Daniel Musngi <mirakelmasim@gmail.com>
2023-03-04 17:36:49 +0800

    Initial MyApplication project

--- app/src/main/java/com/example/myapplication/ListActivity.java
+++ app/src/main/java/com/example/myapplication/ListActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        Spinner spinner = (Spinner) findViewById(R.id.characters);
+    // Create an ArrayAdapter using the string array and a default spinner layout
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.characters, android.R.layout.simple_spinner_item);
+    // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+            spinner.setAdapter(adapter);
+
+
+        //listview=(ListView)findViewById(R.id.listView);
+        //listItem = getResources().getStringArray(R.array.array_technology);
+        //final ArrayAdapter<String> adapter = new ArrayAdapter
     }
 }
