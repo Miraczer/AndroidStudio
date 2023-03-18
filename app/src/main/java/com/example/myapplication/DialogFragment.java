89312f4559f911076973cede9cebc1b95558cbdd
Admin <mirakelmasim@gmail.com>
2023-03-18 17:42:04 +0800

    March 18, 2023 - Dialog Activity

---
+++ app/src/main/java/com/example/myapplication/DialogFragment.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogFragment extends androidx.fragment.app.DialogFragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.activity_dialog_fragment, container, false);
+
+
+    }
+}
